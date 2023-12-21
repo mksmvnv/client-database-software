fcdaf62336d0e6a882aa3aaec3ca02ff794a1655
mksmvnv <mksmvnv@icloud.com>
2023-12-22 00:14:31 +0300

    Update structure prog

--- src/main.c
+++ src/main.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include "add_client.h"
-#include "view_client_list.h"
-#include "search_client.h"
-#include "remove_client.h"
 
-struct Client {
-    int client_id;
-    char first_name[32];
-    char last_name[32];
-    int age;
-    char address[64];
-    char phone_number[20];
-    char email[64];
-};
+#include "add_client.h"
+#include "remove_client.h"
+#include "search_client.h"
+#include "view_client_list.h"
 
 int main() {
     int choice = 0;
     while (choice != 5) {
         printf(
-        "CLIENT DATABASE SOFTWARE\n\n"
-        "1. Add new client\n"
-        "2. View client list\n"
-        "3. Search client\n"
-        "4. Remove client\n"
-        "5. Exit\n");
+            "CLIENT DATABASE SOFTWARE\n\n"
+            "1. Add new client\n"
+            "2. View client list\n"
+            "3. Search client\n"
+            "4. Remove client\n"
+            "5. Exit\n");
         scanf("%d", &choice);
 
         switch (choice) {
@@ -68,7 +58,6 @@ int main() {
                 getchar();
                 system("clear");
                 break;
-
         }
     }
     return 0;
