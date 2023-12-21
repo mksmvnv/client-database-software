ced9e95d06cfe4af5a96a465866edb1a9e1a29de
mksmvnv <mksmvnv@icloud.com>
2023-12-21 10:48:31 +0300

    Update structure prog

--- src/main.c
+++ src/main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-void add_client();
-void view_client_list();
-void search_client();
-void remove_client();
+#include "add_client.h"
+#include "view_client_list.h"
+#include "search_client.h"
+#include "remove_client.h"
 
 struct Client {
     int client_id;
@@ -73,134 +72,4 @@ int main() {
         }
     }
     return 0;
-}
-
-void add_client() {
-    char another;
-    FILE *fp;
-    struct Client info;
-    srand(time(NULL));
-
-    do {
-        system("clear");
-        printf("ADD NEW CLIENT INFO\n\n");
-        fp = fopen("client_info", "a");
-
-        int client_id = rand() % 100000 + 1;
-        info.client_id = client_id;
-        printf("Enter first name: ");
-        scanf("%s", info.first_name);
-        printf("Enter last name: ");
-        scanf("%s", info.last_name);
-        printf("Enter age: ");
-        scanf("%d", &info.age);
-        printf("Enter address: ");
-        scanf("%s", info.address);
-        printf("Enter phone number: ");
-        scanf("%s", info.phone_number);
-        printf("Enter email: ");
-        scanf("%s", info.email);
-
-        if (fp == NULL) {
-            fprintf(stderr, "Can't open file\n\n");
-        }else{
-            printf("Client saved successfully!\n\n");
-        }
-
-        fwrite(&info, sizeof(struct Client), 1, fp);
-        fclose(fp);
-
-        printf("Do you want to add another client? (y/n): ");
-        scanf("%s", &another);
-
-    }while (another == 'y' || another == 'Y');
-}
-
-void view_client_list() {
-    FILE *fp;
-    struct Client info;
-    fp = fopen("client_info", "r");
-    if (fp == NULL) {
-        fprintf(stderr, "Can't open file\n\n");
-    }
-    printf("VIEW CLIENT LIST\n\n");
-
-    while (fread(&info, sizeof(struct Client), 1, fp)) {
-        printf("Client ID: %d\n", info.client_id);
-        printf("Name: %s %s\n", info.first_name, info.last_name);
-        printf("Age: %d\n", info.age);
-        printf("Adress: %s\n", info.address);
-        printf("Phone Number: %s\n", info.phone_number);
-        printf("Email: %s\n\n", info.email);
-    }
-    fclose(fp);
-    getchar();
-}
-
-void search_client() {
-    FILE *fp;
-    struct Client info;
-    int client_id, found = 0;
-    fp = fopen("client_info", "r");
-    if (fp == NULL) {
-        fprintf(stderr, "Can't open file\n\n");
-    }
-    printf("SEARCH CLIENT\n\n");
-    printf("Enter Client ID: ");
-    scanf("%d", &client_id);
-    printf("\n");
-
-    while (fread(&info, sizeof(struct Client), 1, fp)) {
-        if (info.client_id == client_id) {
-            found = 1;
-            printf("Client ID: %d\n", info.client_id);
-            printf("Name: %s %s\n", info.first_name, info.last_name);
-            printf("Age: %d\n", info.age);
-            printf("Adress: %s\n", info.address);
-            printf("Phone Number: %s\n", info.phone_number);
-            printf("Email: %s\n\n", info.email);
-        }
-    }
-    if (!found) {
-        printf("Client not found\n\n");
-    }
-    fclose(fp);
-    getchar();
-
-}
-
-void remove_client() {
-    FILE *fp, *fprem;
-    struct Client info;
-    int client_id, found = 0;
-    printf("REMOVE CLIENT\n\n");
-    fp = fopen("client_info", "r");
-    fprem = fopen("removed_clients", "w");
-    if (fp == NULL) {
-        fprintf(stderr, "Can't open file\n\n");
-    }
-    printf("Enter Client ID: ");
-    scanf("%d", &client_id);
-    printf("\n");
-
-    while (fread(&info, sizeof(struct Client), 1, fp)) {
-        if (info.client_id == client_id) {
-            found = 1;
-        }else {
-            fwrite(&info, sizeof(struct Client), 1, fprem);
-        }
-    }
-    fclose(fp);
-    fclose(fprem);
-
-    if (found) {
-        remove("client_info");
-        rename("removed_clients", "client_info");
-        printf("Client removed successfully!\n\n");
-    }
-    if (!found) {
-        printf("Client not found\n\n");
-    }
-    getchar();
-    remove("removed_clients");
 }
